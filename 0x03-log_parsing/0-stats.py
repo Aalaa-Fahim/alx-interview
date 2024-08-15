74285336c2c19a4eed3f93ec0ce4bbabae76d1d4
Aalaa-Fahim <143665741+Aalaa-Fahim@users.noreply.github.com>
2024-08-15 22:52:32 +0300

    Updated solution

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,17 +2,10 @@
 import sys
 
 
-
 total_size = 0
 count = 0
-dict_sc = {"200": 0,
-           "301": 0,
-           "400": 0,
-           "401": 0,
-           "403": 0,
-           "404": 0,
-           "405": 0,
-           "500": 0}
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
@@ -26,7 +19,6 @@ try:
                 status_codes_dict[status_code] += 1
 
             total_size += file_size
-
             count += 1
 
         if count == 10:
