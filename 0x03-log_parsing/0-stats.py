ed8decbf8c5bb23bb089b0fdbeb564ab16fc5ca8
Aalaa-Fahim <143665741+Aalaa-Fahim@users.noreply.github.com>
2024-08-15 22:54:31 +0300

    Updated solution

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,10 +2,11 @@
 import sys
 
 
-total_size = 0
-count = 0
 status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                      '404': 0, '405': 0, '500': 0}
+
+total_size = 0
+count = 0
 
 try:
     for line in sys.stdin:
